71605ac3768cc972cb50093a63f6c5449c3d6c93
SAORI KODERA <tibezu.dash@gmail.com>
2024-02-03 17:43:50 +0900

    dbを追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -51,7 +51,7 @@
 # ------------------------------------
 server "54.238.164.17",
   user: "ec2-user",
-  roles: %w{web app},
+  roles: %w{web db app},
   ssh_options: {
     user: "ec2-user", # overrides user setting above
     keys: %w(~/Downloads/dic_sample.pem),
